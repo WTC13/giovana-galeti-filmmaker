66a6e1ee63934f60d89241bfb2c96de5b6bb1cca
Wendell Costa <wendell.timoteoc@gmail.com>
2025-06-21 17:22:44 -0300

    create sections marcas and contacts

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,29 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+        loop:true,
+        center:true,
+        margin:100,
+        nav:true,
+        items: 1,
+        dots: true,
+        // autoplay: true,
+        // autoplayTimeout: 3000,
+        // autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+});
